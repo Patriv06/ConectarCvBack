4e6dbdc3b4830641cadfda5a8c7eee05c4240252
Patriv06 <privas06@gmail.com>
2022-06-29 17:36:59 -0300

    crossorigin

--- src/main/java/com/ejemplo/SpringBoot/controller/Controller.java
+++ src/main/java/com/ejemplo/SpringBoot/controller/Controller.java
@@ -60,7 +60,7 @@ public class Controller {
     
     @GetMapping ("/ver/personas")
     @ResponseBody
-    @CrossOrigin(origins ="http: //localhost:4200")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public List <Persona> verPersonas(){
      //   return listaPersonas;
      return persoServ.verPersona();
@@ -71,7 +71,7 @@ public class Controller {
     }
     
     @PutMapping("/modif/persona")
-    @CrossOrigin(origins ="http: //localhost:4200")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void modificarPersona(@RequestBody Persona per){
         persoServ.modificarPersona(per);    }
     
@@ -79,6 +79,7 @@ public class Controller {
     @Autowired
     private IHobbiesService hobbiServ;
     @PostMapping ("/new/hobbies")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void agregarHobbies(@RequestBody Hobbies hobb){
     
      hobbiServ.crearHobbies(hobb);
@@ -86,15 +87,18 @@ public class Controller {
     
     @GetMapping ("/ver/hobbies")
     @ResponseBody
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public List <Hobbies> verH(){
      
      return hobbiServ.verHobbies();
     }
     @DeleteMapping ("/delete/hobbies/{id}")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void borrarHobbie(@PathVariable Long id){
         hobbiServ.borrarHobbies(id);
     }
     @PutMapping("/modif/hobbies")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void modificarHobbie(@RequestBody Hobbies hobb){
         hobbiServ.modificarHobbiues(hobb);    }
     
@@ -102,6 +106,7 @@ public class Controller {
     @Autowired
     private IEstudiosService estServ;
     @PostMapping ("/new/estudios")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void agregarEstudios(@RequestBody Estudios est){
     
      estServ.crearEstudios(est);
@@ -109,15 +114,18 @@ public class Controller {
     
     @GetMapping ("/ver/estudios")
     @ResponseBody
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public List <Estudios> verE(){
      
      return estServ.verEstudios();
     }
     @DeleteMapping ("/delete/estudios/{id}")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void borrarEstudio(@PathVariable Long id){
         estServ.borrarEstudios(id);
     }
     @PutMapping("/modif/estudios")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void modificarEstudio(@RequestBody Estudios estud){
         estServ.modificarEstudios(estud);    }
     
@@ -125,6 +133,7 @@ public class Controller {
     @Autowired
     private IIdiomaService idmServ;
     @PostMapping ("/new/idioma")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void agregarIdioma(@RequestBody Idioma idm){
     
      idmServ.crearIdiomas(idm);
@@ -132,15 +141,18 @@ public class Controller {
     
     @GetMapping ("/ver/idioma")
     @ResponseBody
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public List <Idioma> verI(){
      
      return idmServ.verIdiomas();
     }
     @DeleteMapping ("/delete/idioma/{id}")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void borrarIdioma(@PathVariable Long id){
         idmServ.borrarIdiomas(id);
     }
     @PutMapping("/modif/idioma")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void modificarIdioma(@RequestBody Idioma idio){
        idmServ.crearIdiomas(idio);  }
     
@@ -148,6 +160,7 @@ public class Controller {
     @Autowired
     private ITrabajosService trabServ;
     @PostMapping ("/new/trabajo")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void agregarTrabajos(@RequestBody Trabajos trab){
     
      trabServ.crearTrabajos(trab);
@@ -155,15 +168,18 @@ public class Controller {
     
     @GetMapping ("/ver/trabajo")
     @ResponseBody
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public List <Trabajos> vert(){
      
      return trabServ.verTrabajos();
     }
     @DeleteMapping ("/delete/trabajo/{id}")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void borrarTrabajo(@PathVariable Long id){
         idmServ.borrarIdiomas(id);
     }
     @PutMapping("/modif/trabajo")
+    @CrossOrigin(origins ="https://curriculum-6394f.web.app")
     public void modificarTrabajo(@RequestBody Trabajos trabj){
        trabServ.crearTrabajos(trabj); }
 
