d2b8023901b9f0bcdc0cde09c57597e865ab3bc0
Patriv06 <privas06@gmail.com>
2022-07-08 15:49:32 -0300

    pruba de busqueda

--- src/main/java/com/ejemplo/SpringBoot/service/IEstudiosService.java
+++ src/main/java/com/ejemplo/SpringBoot/service/IEstudiosService.java
@@ -3,6 +3,8 @@ package com.ejemplo.SpringBoot.service;
 
 import com.ejemplo.SpringBoot.model.Estudios;
 import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 
 public interface IEstudiosService {
@@ -15,5 +17,7 @@ public interface IEstudiosService {
     public Estudios buscarEstudios(Long id);
     
     public void modificarEstudios(Estudios estud);
+    
+    public List<Estudios> verEtudiosOrdenados();
     
 }
